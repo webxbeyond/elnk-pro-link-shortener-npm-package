8c6282ef7cd56c86748e27accd759f09fd571011
Anis Afifi <anis@afifi.pro>
2025-02-04 16:30:44 +0600

    2025-02-04

--- src/type.ts
+++ src/type.ts
@@ -1,17 +1,19 @@
 export type ElnkOptions = {
-  message: string;
-  apiKey?: string;
-  apiUrl?: string;
+  apiKey: string;
+  apiUrl: string;
   longUrl?: string;
   shortUrl?: string;
   senderId?: string;
   domainId?: number,
-  endPoints: {
-    links: string;
-    domains: string;
-  }
-}
 
+}
+export type Variables ={
+    apiBaseUrl: string;
+    endPoints: {
+        links: string;
+        domains: string;
+      }
+}
 export type ElnkResponse = {
   success: boolean;
   message?: string;
